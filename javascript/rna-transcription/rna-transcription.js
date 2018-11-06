b69b32b1b31d8b2bb55fe9fdf0cc631ef753fd4d
d-pettersson <david.pettersson@code.berlin>
2018-11-06 20:16:12 +0100

    Added RNA Transcription

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,17 +1,24 @@
-export const toRna = adn => {
-  if (adn === '') {
-    return '';
-  } else {
-    adn.split("").map(function(char) {
-      if (char === 'G') {
-        return 'C';
-      } else if (char === 'C') {
-        return 'G';
-      } else if (char === 'T') {
-        return 'A';
-      } else if (char === 'A') {
-        return 'U';
-      }
-    }.join(""))
+const translations = {
+  G : 'C',
+  C : 'G',
+  T : 'A',
+  A : 'U'
+};
+
+export function toRna(strand) {
+  if (strand === '') {
+    return ('');
+  } else if (/G|C|T|A/g.test(strand)) {
+    if (/X|U/g.test(strand)) {
+      throw new Error ('Invalid input DNA.');
+    } else {
+      return strand.split('').map(translate).join('');
+    }
+  } else if (/X|U/g.test(strand)) {
+      throw new Error ('Invalid input DNA.');
   }
 };
+
+const translate = nucleotides => {
+  return translations[nucleotides]
+}
