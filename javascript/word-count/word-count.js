5ee79bebada9e19825554dc0cff6b7eed0d5a208
d-pettersson <david.pettersson@code.berlin>
2018-11-20 15:08:08 +0100

    Added word-count

---
+++ javascript/word-count/word-count.js
@@ -0,0 +1,15 @@
+export default class Word {
+  constructor(string) {
+    this.string = string;
+  }
+
+  count(str) {
+    let splitString = str.toLowerCase().replace(/\n|\t/, ' ');
+    splitString = splitString.replace(/^\s*/, '').replace(/\s*$/, '').split(' ');
+    let count = {};
+    let filteredCount = {};
+    filteredCount = splitString.filter(x => x.length > 0).forEach(function(x) {count[x] = (count[x] | 0) + 1})
+
+    return count;
+  }
+}
